fa4f4b404f6332ab6d627f9b99cf4119532e40ee
devendra <devendraprasadnayak@gmail.com>
2019-04-14 11:08:14 +0530

    initial

--- FileProcessing/com/project/FileProcessing.py
+++ FileProcessing/com/project/FileProcessing.py
@@ -1,1 +1,12 @@
 # this is for all type of file reading and processing
+import pyspark
+from pyspark.sql import SparkSession
+from kafka import KafkaConsumer
+
+spark = SparkSession.builder.appName("pysparkTest").master("local[*]").getOrCreate()
+
+
+df=spark.read.format("csv").option("header", "true").csv("D:\\Software Projects\\Pycharm\\FirstProject\\com\\rxcorp\\testFile.csv")
+#spark.read.option("header", "true").csv("/path/to_csv.csv")
+
+df.show(20)
